cb13acd75e4f92dda97ad3dd2cc623acb0aea70b
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2021-12-29 20:53:21 +0530

    Data Added for API

--- server.js
+++ server.js
@@ -5,53 +5,16 @@ const fs = require('fs');
 
 fs.readFile("data.json","UTF-8",(err,fileData)=>{
     if(err) throw err;
-
+    
+    app.get("/",function(request,response)
+    {
+    response.send(fileData);
+    });
+    
     console.log(fileData);
 });
 
-app.get("/",function(request,response)
-{
-response.send([
-    {
-    "id": "1",
-    "motorStart": "1032,50",
-    "motorEnd": "1032,50",
-    "TxStartAt": "06:31:59,16.12.21",
-    "TxEndAt": "07:31:39,16.12.21",
-    "totalUtilization": "0,50",
-    "cost": "0,14"
-},
-{
-    "id": "2",
-    "motorStart": "1032,50",
-    "motorEnd": "1032,50",
-    "TxStartAt": "06:31:59,16.12.21",
-    "TxEndAt": "07:31:39,16.12.21",
-    "totalUtilization": "0,50",
-    "cost": "0,14"
-},
-{
-    "id": "3",
-    "motorStart": "1032,50",
-    "motorEnd": "1032,50",
-    "TxStartAt": "06:31:59,16.12.21",
-    "TxEndAt": "07:31:39,16.12.21",
-    "totalUtilization": "0,50",
-    "cost": "0,14"
-},
-{
-    "id": "4",
-    "motorStart": "1032,50",
-    "motorEnd": "1032,50",
-    "TxStartAt": "06:31:59,16.12.21",
-    "TxEndAt": "07:31:39,16.12.21",
-    "totalUtilization": "0,50",
-    "cost": "0,14"
-}
-]);
-
-});
-
+    
 app.listen(3000,function(){
     console.log("Server starts at port 3000");
 })
